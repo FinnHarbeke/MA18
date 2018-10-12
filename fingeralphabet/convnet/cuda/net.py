c20e9c9733b860e2bec5e20f1e553e2e26e67444
FinnHarbeke <finn.harbeke@gmail.com>
2018-10-12 08:23:11 +0200

    added cuda folder for rendering with gpu

---
+++ fingeralphabet/convnet/cuda/net.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+
+class FingeralphabetNet(nn.Module):
+
+    def __init__(self):
+        super(FingeralphabetNet, self).__init__()
+        self.cuda0 = torch.device('cuda:0')
+        # 3 input image channel, 6 output channels, 5x5 square convolution
+        # kernel
+        self.conv1 = nn.Conv2d(3, 10, (32, 24), stride=2)
+        self.conv2 = nn.Conv2d(10, 25, (16, 12))
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(13950, 1200)
+        self.fc2 = nn.Linear(1200, 150)
+        self.fc3 = nn.Linear(150, 29)
+        self.criterion = nn.CrossEntropyLoss().to(device=self.cuda0)
+        self.optim = torch.optim.Adam(self.parameters(), 1e-4)
+        
+
+    def forward(self, x):
+        # Max pooling over a (2, 2) window
+        x = x.to(device=self.cuda0)
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # If the size is a square you can only specify a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+    def train(self, dataloader, every_batch=2000, save_path=None):
+        """
+        train function using a torch.utils.data.DataLoader()
+            :param dataloader: torch.utils.data.DataLoader()
+            :param every_batch=2000: after how many batches to report error etc.
+            :param save_path=None: where to save the net, if None FingeralphabetNet is not savec during training
+        """   
+        running_error = 0
+        for i_batch, sample_batched in enumerate(dataloader):
+            X = sample_batched['image_tensor'].to(device=self.cuda0)
+            y = sample_batched['target_ind'].to(device=self.cuda0)
+            outputs = self(X)
+            error = self.criterion(outputs, y)
+            self.backprop(error)
+            running_error += error
+
+            if i_batch % every_batch == 0:
+                first = max(0, i_batch - every_batch)
+                print('Error of batches {} to {}:'.format(first, i_batch), running_error/(X.shape[0]*(every_batch)))
+                print('Trained {} Batches of size {}!'.format(i_batch, X.shape[0]))
+                if save_path:
+                        torch.save(self.state_dict(), save_path)
+                running_error = 0
+
+
+
+
+
+    def backprop(self, error, learning_rate=0.01):
+        self.zero_grad()
+        error.backward(retain_graph=False)
+
+        #update
+
+        # for f in self.parameters():
+            # f.data.sub_(f.grad.data * learning_rate)
+        self.optim.step()
+
+
+def letter(arr):
+    if arr.size()[-1] != 29:
+        raise ValueError('array length gotta be 29!!')
+
+    ind = 0
+    for i in range(29):
+        ind = i if arr[0][i] > arr[0][ind] else ind
+
+    abc = [chr(i) for i in range(ord("A"), ord("Z")+1)] + ["SCH", "CH", "NOTHING"]
+    return abc[ind]
